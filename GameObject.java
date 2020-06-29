d96f8bbc71d4388b1b4dbd08454a6b2882d84809
redvsdead <56507988+redvsdead@users.noreply.github.com>
2020-06-29 14:19:26 +0300

    initial commit

---
+++ GameObject.java
@@ -0,0 +1,14 @@
+package com.javarush.games.snake;
+import com.javarush.engine.cell.*;
+
+public class GameObject {
+
+    public int x;
+    public int y;
+
+    public GameObject(int _x, int _y) {
+        x = _x;
+        y = _y;
+    }
+
+}
